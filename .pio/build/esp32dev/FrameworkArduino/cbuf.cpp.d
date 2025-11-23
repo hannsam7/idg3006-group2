51c3d3bdaff0a1940f39bad9433b8309dd7d1e6c
hannsam7 <hannsam@stud.ntnu.no>
2025-11-23 15:45:28 +0100

    new c++

---
+++ .pio/build/esp32dev/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/esp32dev/FrameworkArduino/cbuf.cpp.o: \
+ /Users/hannasamborska/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ /Users/hannasamborska/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
---
+++ .pio/build/esp32dev/libe96/WebSockets/libsha1/libsha1.c.d
@@ -0,0 +1,2 @@
+.pio/build/esp32dev/libe96/WebSockets/libsha1/libsha1.c.o: \
+ .pio/libdeps/esp32dev/WebSockets/src/libsha1/libsha1.c
