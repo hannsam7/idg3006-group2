67a356331063865cf3f46b74fba23533cb4823a4
hannsam7 <hannsam@stud.ntnu.no>
2025-11-23 17:14:54 +0100

    changing logic to have a lot prepared before esp and mmwave connection is done

--- esp32/src/main.cpp
+++ esp32/src/main.cpp
@@ -6,8 +6,8 @@
 #define TX_PIN 17
 
 // WiFi credentials
-const char* ssid     = "YOUR_WIFI_SSID";
-const char* password = "YOUR_WIFI_PASSWORD";
+const char* ssid     = "YOUR_REAL_SSID";
+const char* password = "YOUR_REAL_PASSWORD";
 
 // WebSocket server (Raspberry Pi)
 WebSocketsClient webSocket;
