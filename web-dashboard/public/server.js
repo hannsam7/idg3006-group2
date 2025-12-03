4330d05344afad66a27598d15b27938a9948e11c
hannsam7 <hannsam@stud.ntnu.no>
2025-12-03 12:46:39 +0100

    changes to commit

--- web-dashboard/public/server.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname  = path.dirname(__filename);
-
-app.use(express.static(path.join(__dirname, "../../web-dashboard/public")));
-
-// ... rest of your server code
